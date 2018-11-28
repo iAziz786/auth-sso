4dc8bf4985187a76a1ca897f4e11148188a3ec64
Mohammad Aziz <mdaziz067@gmail.com>
2018-11-29 01:58:58 +0530

    Integrade: pretter with eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,17 @@
 module.exports = {
-  semi: false,  
   arrowParens: "always",
-  singleQuote: false
-};
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  parser: "babylon",
+  printWidth: 80,
+  proseWrap: "preserve",
+  requirePragma: false,
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false
+}
