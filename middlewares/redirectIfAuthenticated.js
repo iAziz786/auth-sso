3c71c06d4231107f7cbf83a18db57e759a351738
Mohammad Aziz <mdaziz067@gmail.com>
2018-11-30 00:36:58 +0530

    Update: redirect to back to the user if logged in

--- middlewares/redirectIfAuthenticated.js
+++ middlewares/redirectIfAuthenticated.js
@@ -1,7 +1,6 @@
 function redirectIfAuthenticated(req, res, next) {
   if (req.isAuthenticated && req.isAuthenticated()) {
-    const redirectTo = req.redirectTo || "/"
-    return res.redirect(redirectTo)
+    return res.redirect("back")
   }
   next()
 }
