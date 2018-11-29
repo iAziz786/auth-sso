4c77facf6a31222d810cdaff41740d7569d9932c
Mohammad Aziz <mdaziz067@gmail.com>
2018-11-30 04:11:39 +0530

    Fix: POST /login not getting called

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const { Router } = require("express")
 const router = Router()
 
 const staticRouter = require("./staticRouter")
-const authRouter = require('./authRouter')
+const authRouter = require("./authRouter")
 const apiRouter = require("./apiRouter")
 
-router.use(staticRouter)
 router.use(authRouter)
+router.use(staticRouter)
 router.use("/api", apiRouter)
 
 module.exports = router
