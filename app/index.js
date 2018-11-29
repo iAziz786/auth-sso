3409962b1636de87691a18b4ce457c0f3193284a
Mohammad Aziz <mdaziz067@gmail.com>
2018-11-29 23:56:52 +0530

    Move: code to app.js

--- app/index.js
+++ app/index.js
@@ -9,40 +9,42 @@ const bodyParser = require("body-parser")
 const logger = require("morgan")
 const dotenv = require("dotenv")
 
-const { User } = require("../components/user/model")
-
 dotenv.config({ path: path.resolve(__dirname, "../.env") })
 
 const router = require("../routes")
 const app = express()
 const publicPath = path.join(__dirname, "../public")
 
-app.set("port", process.env.PORT)
+app.set("port", process.env.PORT || 3000)
+app.set("views", "./views")
+app.set("view engine", "pug")
 
 // app.use(function(req, res, next) {
-  //     res.setHeader("Content-Security-Policy", "default-src 'self' feedwee.herokuapp.com");
-  //     return next();
-  // });
-  
-  require("../services/passport")
-  
-  app.use(cookieParser())
-  app.use(cookieSession({
-    name: 'session',
-    keys: ['talking to the moon'],
-    maxAge: ms('30 days')
-  }))
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: false }))
-  // NOTE: Any middlware starts with express.(...) will be called before any
-  // middlware which get called with passport.(...). Reason why it happens
-  // is not found yet by me
-  app.use(express.static(publicPath))
-  app.use(passport.initialize())
-  app.use(passport.session())
-  // app.use(cors())
-  app.use(logger("dev"))
-  
-  app.use("/", router)
+//     res.setHeader("Content-Security-Policy", "default-src 'self' feedwee.herokuapp.com");
+//     return next();
+// });
+
+require("../services/passport")
+
+app.use(cookieParser())
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["talking to the moon"],
+    maxAge: ms("30 days")
+  })
+)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// NOTE: Any middlware starts with express.(...) will be called before any
+// middlware which get called with passport.(...). Reason why it happens
+// is not found yet by me
+app.use(express.static(publicPath))
+app.use(passport.initialize())
+app.use(passport.session())
+// app.use(cors())
+app.use(logger("dev"))
+
+app.use("/", router)
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const http = require("http")
 const chalk = require("chalk").default
-const passport = require('passport');
 
 const app = require("./app")
-
-app.set("views", "./views")
-app.set("view engine", "pug")
-
-const PORT = process.env.PORT || 3000
 
 const server = http.createServer(app)
 
 server.listen(app.get("port"), () => {
-  console.log(chalk.green(`App is running on port http://localhost:${PORT}`))
+  console.log(
+    chalk.green(`App is running on port http://localhost:${app.get("port")}`)
+  )
 })
 
 module.exports = server
