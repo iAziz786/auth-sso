37770dbdd763b7bb80ad3d01bc2f1902e0dfb689
Mohammad Aziz <mdaziz067@gmail.com>
2018-11-30 04:02:13 +0530

    Add: unhandleRejection

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require("http")
 const chalk = require("chalk").default
 
+process.on("unhandledRejection", (reason) => {
+  console.error(reason)
+  process.exit(1)
+})
+
 const app = require("./app")
 
 const server = http.createServer(app)
