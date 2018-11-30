407bf5735b6ee7eac5f41f84fc07f39027ebd8b0
Mohammad Aziz <mdaziz067@gmail.com>
2018-11-30 16:33:12 +0530

    Making userId more dynamic

--- middlewares/registerNewClient.js
+++ middlewares/registerNewClient.js
@@ -1,11 +1,11 @@
-const {
-  Types: { ObjectId }
-} = require("mongoose")
 const { Client } = require("../components/client/modal")
 const createPassword = require("../utils/createPassword")
 
 function registerNewClient() {
   return async (req, res, next) => {
+    const {
+      Types: { ObjectId }
+    } = require("mongoose")
     try {
       await Client.create({
         clientId: ObjectId(),
