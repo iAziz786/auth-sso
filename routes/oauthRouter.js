f8383cf0dc87d478df35cddfa3f5f76f93c62e86
Mohammad Aziz <mdaziz067@gmail.com>
2018-12-05 11:53:33 +0530

    Passing back the state

--- routes/oauthRouter.js
+++ routes/oauthRouter.js
@@ -11,14 +11,14 @@ oauthRouter.get(
   "/oauth/authorize",
   oauthServer.authorization,
   async (req, res) => {
-    const { client_id, redirect_uri } = req.query
+    const { client_id, redirect_uri, state } = req.query
     console.log(client_id)
     try {
       const client = await Client.findById(client_id)
       console.log({ client })
       // if (mathRedirectUri(client, redirect_uri)) {
       const code = generateToken()
-      return res.redirect(`${redirect_uri}?code=${code}`)
+      return res.redirect(`${redirect_uri}?code=${code}&state=${state}`)
       // }
       return res.render("404")
     } catch (err) {
