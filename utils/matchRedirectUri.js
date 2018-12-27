16e2949c5b819c0b373893635abcc408dc52a758
Mohammad Aziz <mdaziz067@gmail.com>
2018-12-27 21:04:34 +0530

    Register URI and redirect URI would be exactly same
    According to the OpenID Connect specification registred redirect urls and redirect should match exactly the same

--- utils/matchRedirectUri.js
+++ utils/matchRedirectUri.js
@@ -1,6 +1,3 @@
-const url = require("url")
-const slashEndValue = require("./slashEndValue")
-
 /**
  * @author Mohammad Aziz
  * @param {Array<string>} clientRedirectUrls
@@ -16,20 +13,8 @@ function mathcRedirectUri(clientRedirectUrls, redirectUrl) {
     clientRedirectUrls = [clientRedirectUrls]
   }
 
-  return clientRedirectUrls.some((redirect) => {
-    const clientUrl = url.parse(redirect)
-    const testUrl = url.parse(redirectUrl)
-    if (clientUrl.pathname.endsWith("/")) {
-      clientUrl.path = slashEndValue(clientUrl.path)
-    }
-    if (testUrl.pathname.endsWith("/")) {
-      redirectUrl.path = slashEndValue(redirectUrl.path)
-    }
-    return (
-      clientUrl.protocol === testUrl.protocol &&
-      clientUrl.host === testUrl.host &&
-      clientUrl.path === testUrl.path
-    )
+  return clientRedirectUrls.some((registeredUri) => {
+    return registeredUri === redirectUrl
   })
 }
 
